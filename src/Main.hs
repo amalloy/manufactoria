62ca8e2e0bf6b18b95a39269ec497eecf8a89557
Alan Malloy <alan@malloys.org>
2021-10-24 02:01:03 -0700

    Generate colorless factories

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ import qualified Data.IntMap as M
 import qualified Data.Sequence as Seq
 import qualified Data.Set as S
 import Control.Applicative ((<|>))
-import Control.Monad (replicateM)
+import Control.Monad (replicateM, guard)
 import Control.Monad.Trans.State.Lazy
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Class
@@ -25,10 +25,11 @@ data Color = Red | Blue | Green | Yellow deriving (Enum, Eq, Ord, Show, Read)
 data ArrowColor = AnyColor | ColoredArrow Color deriving (Eq, Ord, Show, Read)
 type Tape = Seq.Seq Color
 
-data Directed a = Directed Destination a
-data Three a = Three { left, straight, right :: a }
+data Directed a = Directed Destination a deriving (Eq, Ord, Show, Read)
+data Three a = Three { left, straight, right :: a } deriving (Eq, Ord, Show, Read)
 data Node stamper scanner = OneWay (Directed stamper) | ThreeWay (Three (Directed stamper))
-data Indexed a = Indexed Index a
+                          deriving (Eq, Ord, Show, Read)
+data Indexed a = Indexed Index a deriving (Eq, Ord, Show, Read)
 
 data ScanResult = ScanResult {consume :: Bool, next :: Destination} deriving (Eq, Ord, Show, Read)
 data Arrow = Arrow ArrowColor ScanResult deriving (Eq, Ord, Show, Read)
@@ -83,36 +84,58 @@ data LayoutState = LayoutState { pending, placed, open :: S.Set Index }
 type Search a = ReaderT ProblemStatement [] a
 
 layouts :: Int -> Search [Indexed (Node () ())]
-layouts n = go $ LayoutState (S.singleton 0) mempty mempty
+layouts n = go $ LayoutState { pending = S.singleton 0
+                             , placed = mempty
+                             , open = S.fromList [1..n]
+                             }
   where go state = case first (pending state) of
           Nothing -> pure []
           Just nodeIndex -> do
             let state' = state { pending = S.delete nodeIndex (pending state)
                                , placed = S.insert nodeIndex (placed state)
                                }
-            (state'', object) <- layoutScanner state' <|> layoutStamper state' nodeIndex
+            (state'', object) <- layoutScanner state' nodeIndex <|> layoutStamper state' nodeIndex
             (Indexed nodeIndex object :) <$> go state''
 
+allowedExits :: Search Destination
+allowedExits = do
+  exits <- asks permittedExits
+  lift $ Terminate <$> exits
+
+reachable :: LayoutState -> [Index]
+reachable state = toList (pending state) ++ toList (placed state)
+
+explore :: LayoutState -> Search (LayoutState, Destination)
+explore state = lift $ do
+  let opens = open state
+  dst <- toList opens
+  pure (state { pending = S.insert dst (pending state)
+              , open = S.delete dst opens
+              }
+        , Goto dst)
+
+
 layoutStamper :: LayoutState -> Index -> Search (LayoutState, Node () ())
-layoutStamper state self = fmap (fmap plainStamper) (reuse <|> placeNew)
+layoutStamper state self = fmap (fmap plainStamper) (reuse <|> explore state)
   where plainStamper dst = OneWay (Directed dst ())
         reuse :: Search (LayoutState, Destination)
-        reuse = (state,) <$> do
-          exits <- asks permittedExits
-          lift $ [Goto dst | dst <- toList (pending state) ++ toList (placed state)
-                           , dst /= self] -- self-pointing scanners are never productive
-            <|> [Terminate exit | exit <- exits]
-        placeNew = lift $ do
-          let opens = open state
-          dst <- toList opens
-          pure (state { pending = S.insert dst (pending state)
-                      , open = S.delete dst opens
-                      }
-                , Goto dst)
+        reuse = (state,) <$>
+                (allowedExits <|>
+                 lift [Goto dst | dst <- reachable state
+                                , dst /= self]) -- self-pointing scanners are never productive)
 
+layoutScanner :: LayoutState -> Index -> Search (LayoutState, Node () ())
+layoutScanner state self = do
+  allowSelfArrows <- asks $ not . allScannersHaveWhiteArrow
+  (state', s) <- choose state
+  guard $ allowSelfArrows || (s /= Goto self)
+  (state'', l) <- choose state'
+  (state''', r) <- choose state''
+  pure (state''', ThreeWay (Three (Directed l ()) (Directed s ()) (Directed r ())))
+  where choose state = ((state,) <$> reuse) <|> explore state
+          where reuse :: Search Destination
+                reuse = allowedExits <|> lift [Goto dst | dst <- reachable state]
 
-layoutScanner :: LayoutState -> Search (LayoutState, Node () ())
-layoutScanner = _
 
 main :: IO ()
-main = interact $ (show . length . factoriesUsing [Accept] [Red, Blue] [scanner1] . read)
+main = interact $ (show . length . flip runReaderT (ProblemStatement [Accept] [] [] True) . layouts . read)
